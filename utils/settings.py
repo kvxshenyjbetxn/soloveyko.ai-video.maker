0a2c264439239add333b4ad25fea7e42c6fd429f
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2025-11-26 16:51:45 +0000

    0.3.0 - реалізовано вкладку Мови

--- utils/settings.py
+++ utils/settings.py
@@ -27,7 +27,7 @@ class SettingsManager:
     def save_settings(self):
         os.makedirs(os.path.dirname(self.settings_file), exist_ok=True)
         with open(self.settings_file, 'w', encoding='utf-8') as f:
-            json.dump(self.settings, f, indent=4)
+            json.dump(self.settings, f, indent=4, ensure_ascii=False)
 
     def get(self, key, default=None):
         if default is None:
