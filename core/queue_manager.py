a497606f4afb7492cab2a94c4f77347eb5a740b8
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2025-11-27 17:41:48 +0000

    0.7.2 - додано можливість видаляти завдання повністью, а також окремо кожне завдання та мови

--- core/queue_manager.py
+++ core/queue_manager.py
@@ -16,3 +16,41 @@ class QueueManager(QObject):
 
     def get_task_count(self):
         return len(self.tasks)
+
+    def get_job(self, job_id):
+        for job in self.tasks:
+            if job['id'] == job_id:
+                return job
+        return None
+
+    def delete_job(self, job_id):
+        job_to_delete = None
+        for job in self.tasks:
+            if job['id'] == job_id:
+                job_to_delete = job
+                break
+        if job_to_delete:
+            self.tasks.remove(job_to_delete)
+            return True
+        return False
+
+    def delete_language_from_job(self, job_id, lang_id):
+        for job in self.tasks:
+            if job['id'] == job_id:
+                if lang_id in job['languages']:
+                    del job['languages'][lang_id]
+                    # If no languages are left, should we delete the job?
+                    # For now, let's leave the job, it will just be empty.
+                    return True
+        return False
+
+    def delete_stage_from_language(self, job_id, lang_id, stage_key):
+        for job in self.tasks:
+            if job['id'] == job_id:
+                if lang_id in job['languages']:
+                    if stage_key in job['languages'][lang_id]['stages']:
+                        job['languages'][lang_id]['stages'].remove(stage_key)
+                        # If no stages are left, should we delete the language?
+                        # For now, let's leave it.
+                        return True
+        return False
