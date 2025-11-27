8572f932fd9b105434d8449a5be1dcb5551f0cee
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2025-11-27 17:02:59 +0000

    0.7.0 - перероблено вкладку черга завдань, тепер це карточки завдання

--- gui/queue_tab.py
+++ gui/queue_tab.py
@@ -1,12 +1,34 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QPushButton, QHBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QLabel, QScrollArea, QGroupBox
+from PySide6.QtCore import Qt
 from utils.translator import translator
+from utils.flow_layout import FlowLayout
+
+class TaskCard(QGroupBox):
+    """A widget that displays a single task as a card."""
+    def __init__(self, job, parent=None):
+        super().__init__(job['name'], parent)
+        self.setMinimumWidth(250)
+        self.setMaximumWidth(350)
+
+        layout = QVBoxLayout(self)
+        
+        for lang_id, lang_data in job['languages'].items():
+            lang_label = QLabel(f"<b>{lang_data['display_name']}</b>")
+            layout.addWidget(lang_label)
+            
+            # Use a single label with rich text for stages
+            stages_text = "<br>".join(f"&nbsp;&nbsp;• {translator.translate(stage_key)}" for stage_key in lang_data['stages'])
+            stages_label = QLabel(stages_text)
+            stages_label.setWordWrap(True)
+            stages_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+            layout.addWidget(stages_label)
+        
+        layout.addStretch()
 
 class QueueTab(QWidget):
     def __init__(self, main_window=None):
         super().__init__()
         self.main_window = main_window
-        self.jobs_expanded = False
-        self.all_expanded = False
         self.init_ui()
 
     def init_ui(self):
@@ -16,89 +38,38 @@ class QueueTab(QWidget):
         self.balance_label = QLabel()
         top_layout.addWidget(self.balance_label)
         top_layout.addStretch()
-        main_layout.addLayout(top_layout)
         
-        # Button layout
-        button_layout = QHBoxLayout()
-        self.expand_jobs_button = QPushButton()
-        self.expand_jobs_button.clicked.connect(self.toggle_expand_jobs)
-        button_layout.addWidget(self.expand_jobs_button)
-
-        self.expand_all_button = QPushButton()
-        self.expand_all_button.clicked.connect(self.toggle_expand_all)
-        button_layout.addWidget(self.expand_all_button)
-        button_layout.addStretch()
-
         self.start_processing_button = QPushButton()
-        button_layout.addWidget(self.start_processing_button)
+        top_layout.addWidget(self.start_processing_button)
+        main_layout.addLayout(top_layout)
 
-        main_layout.addLayout(button_layout)
-
-        self.task_tree = QTreeWidget()
-        self.task_tree.setHeaderLabels([translator.translate('task_header')])
-        self.task_tree.setIndentation(10)
-        self.task_tree.setStyleSheet("QTreeView::item { padding: 1px 0px; }")
-        main_layout.addWidget(self.task_tree)
+        # Scroll Area for task cards
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         
-        self.retranslate_ui() # Set initial button text
+        scroll_content = QWidget()
+        self.tasks_layout = FlowLayout(scroll_content)
+        
+        scroll_area.setWidget(scroll_content)
+        main_layout.addWidget(scroll_area)
+        
+        self.retranslate_ui()
 
     def add_task(self, job):
-        # Create top-level item for the job
-        job_item = QTreeWidgetItem(self.task_tree, [job['name']])
-
-        # Create child items for each language
-        for lang_id, lang_data in job['languages'].items():
-            lang_item = QTreeWidgetItem(job_item, [lang_data['display_name']])
-            
-            # Create grand-child items for each stage
-            for stage_key in lang_data['stages']:
-                stage_name = translator.translate(stage_key)
-                stage_item = QTreeWidgetItem(lang_item, [stage_name])
-        
-        # Reset state and expand to job level
-        self.jobs_expanded = False 
-        self.all_expanded = False
-        self.toggle_expand_jobs()
-
-    def toggle_expand_jobs(self):
-        if self.jobs_expanded:
-            self.task_tree.collapseAll()
-            self.jobs_expanded = False
-            self.all_expanded = False
-        else:
-            self.task_tree.collapseAll()
-            for i in range(self.task_tree.topLevelItemCount()):
-                self.task_tree.topLevelItem(i).setExpanded(True)
-            self.jobs_expanded = True
-            self.all_expanded = False # Expanding jobs is a subset of expanding all
-        self.update_button_text()
-            
-    def toggle_expand_all(self):
-        if self.all_expanded:
-            self.task_tree.collapseAll()
-            self.all_expanded = False
-            self.jobs_expanded = False
-        else:
-            self.task_tree.expandAll()
-            self.all_expanded = True
-            self.jobs_expanded = True
-        self.update_button_text()
-
-    def update_button_text(self):
-        if self.jobs_expanded:
-            self.expand_jobs_button.setText(translator.translate('collapse_jobs'))
-        else:
-            self.expand_jobs_button.setText(translator.translate('expand_jobs'))
-            
-        if self.all_expanded:
-            self.expand_all_button.setText(translator.translate('collapse_all'))
-        else:
-            self.expand_all_button.setText(translator.translate('expand_all'))
+        task_card = TaskCard(job)
+        self.tasks_layout.addWidget(task_card)
 
     def update_balance(self, balance_text):
         self.balance_label.setText(balance_text)
 
     def retranslate_ui(self):
-        self.task_tree.setHeaderLabels([translator.translate('task_header')])
         self.start_processing_button.setText(translator.translate('start_processing'))
-        self.update_button_text()
+        # We might need to retranslate existing cards if language changes
+        for i in range(self.tasks_layout.count()):
+            widget = self.tasks_layout.itemAt(i).widget()
+            if isinstance(widget, TaskCard):
+                # This is complex, as the card is built with translated text.
+                # For now, we assume cards are not re-translated after creation.
+                pass
---
+++ utils/flow_layout.py
@@ -0,0 +1,90 @@
+import sys
+from PySide6.QtCore import QPoint, QRect, QSize, Qt
+from PySide6.QtWidgets import QApplication, QLayout, QLayoutItem, QPushButton, QWidget
+
+
+class FlowLayout(QLayout):
+    def __init__(self, parent=None, margin=-1, hSpacing=-1, vSpacing=-1):
+        super().__init__(parent)
+
+        self.itemList = []
+        self.m_hSpace = hSpacing
+        self.m_vSpace = vSpacing
+        self.setContentsMargins(margin, margin, margin, margin)
+
+    def __del__(self):
+        while self.itemList:
+            item = self.itemList.pop()
+            del item
+
+    def addItem(self, item):
+        self.itemList.append(item)
+
+    def count(self):
+        return len(self.itemList)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList[index]
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList.pop(index)
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientation(0)
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        return self._doLayout(QRect(0, 0, width, 0), True)
+
+    def setGeometry(self, rect):
+        super().setGeometry(rect)
+        self._doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+        for item in self.itemList:
+            size = size.expandedTo(item.minimumSize())
+        
+        margin = self.contentsMargins()
+        size += QSize(margin.left() + margin.right(), margin.top() + margin.bottom())
+        return size
+
+    def _doLayout(self, rect, testOnly):
+        left, top, right, bottom = self.getContentsMargins()
+        effectiveRect = rect.adjusted(left, top, -right, -bottom)
+        x = effectiveRect.x()
+        y = effectiveRect.y()
+        lineHeight = 0
+
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.spacing()
+            if self.m_hSpace >= 0:
+                spaceX = self.m_hSpace
+            spaceY = self.spacing()
+            if self.m_vSpace >= 0:
+                spaceY = self.m_vSpace
+
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > effectiveRect.right() and lineHeight > 0:
+                x = effectiveRect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+
+            if not testOnly:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+
+        return y + lineHeight - effectiveRect.y() + bottom
