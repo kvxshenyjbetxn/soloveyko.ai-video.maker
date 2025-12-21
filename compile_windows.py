1fb89691966e7019ac4cfcb28b6013ce4238ead0
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2025-12-21 12:39:10 +0000

    0.37.7 - спроба виправити конфіг для мак

--- compile_windows.py
+++ compile_windows.py
@@ -10,7 +10,7 @@ def compile_project():
     # ------------------------------------
 
     # Назва вихідного файлу
-    app_name = "CombainAI v0.37.6"
+    app_name = "CombainAI v0.37.7"
     
     # Перевірка наявності PyInstaller та залежностей
     try:
