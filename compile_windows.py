e8ea7954b2cabb0d70e2921605408f0882bdd212
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2025-12-22 12:33:22 +0000

    0.2.7 - зміна назви

--- compile_windows.py
+++ compile_windows.py
@@ -10,7 +10,7 @@ def compile_project():
     # ------------------------------------
 
     # Назва вихідного файлу
-    app_name = "CombainAI v0.37.10"
+    app_name = "Soloveyko.AI-Video.Maker v0.2.7"
 
     # Перевірка наявності PyInstaller та залежностей
     try:
