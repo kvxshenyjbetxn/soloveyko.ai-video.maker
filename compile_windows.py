de12cbeb149f4d5cb05213399121501e35aecfa5
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2025-12-31 16:15:36 +0000

    0.39.8 - збільшено кількість повторних спроб для войсмейкера до 10

--- compile_windows.py
+++ compile_windows.py
@@ -10,7 +10,7 @@ def compile_project():
     # ------------------------------------
 
     # Назва вихідного файлу
-    app_name = "Soloveyko.AI-Video.Maker v0.39.7"
+    app_name = "Soloveyko.AI-Video.Maker v0.39.8"
 
     # Перевірка наявності PyInstaller та залежностей
     try:
