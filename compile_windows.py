21b199ec43dad724d2ddab94d316981c76ca22c3
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2026-01-04 18:10:12 +0000

    0.44.13 - виправленя запуску программи, була відсутня бібліотека

--- compile_windows.py
+++ compile_windows.py
@@ -10,7 +10,7 @@ def compile_project():
     # ------------------------------------
 
     # Назва вихідного файлу
-    app_name = "Soloveyko.AI-Video.Maker v0.44.12"
+    app_name = "Soloveyko.AI-Video.Maker v0.44.13"
 
     # Перевірка наявності PyInstaller та залежностей
     try:
