578777390b781c70658f0fd2169c75a8deafbc4c
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2026-01-04 15:11:01 +0000

    0.44.5 - Налаштування\API\Зображення\ElevenLabsImage

--- compile_windows.py
+++ compile_windows.py
@@ -10,7 +10,7 @@ def compile_project():
     # ------------------------------------
 
     # Назва вихідного файлу
-    app_name = "Soloveyko.AI-Video.Maker v0.44.4"
+    app_name = "Soloveyko.AI-Video.Maker v0.44.5"
 
     # Перевірка наявності PyInstaller та залежностей
     try:
