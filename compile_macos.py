ac283e390916eb3e62344524fbac7a145c66d65f
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2026-01-04 01:27:13 +0000

    0.43.2 - додано окрему вкалдку edgetts та пояснення що для цього сервісу не потрібні налаштування ата апі ключі

--- compile_macos.py
+++ compile_macos.py
@@ -13,7 +13,7 @@ def compile_project():
     # ------------------------------------
 
     # Назва вихідного файлу (синхронізовано з Windows)
-    app_name = "Soloveyko.AI-Video.Maker v0.43.1"
+    app_name = "Soloveyko.AI-Video.Maker v0.43.2"
     
     # Перевірка наявності PyInstaller та залежностей
     try:
