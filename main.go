00ed5d7c9f80fe1183d1cb12f2655b5c9d1d2540
kvxshenyjbetxn <64891729+kvxshenyjbetxn@users.noreply.github.com>
2026-02-23 23:22:20 +0000

    0.19.6.1 - мінор

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 
-		Resizable: true,
+		DisableResize: false,
 		Mac: &mac.Options{
 			TitleBar:             mac.TitleBarDefault(),
 			Appearance:           mac.NSAppearanceNameDarkAqua,
